6ea7f1445ecb11dff76a7d382ce105d99c8d6904
Nikhil <nirhalnihar8344@gmail.com>
2026-01-02 17:32:09 +0530

    Add Procfile and update app.py, requirements.txt

--- app.py
+++ app.py
@@ -35,4 +35,5 @@ def logout():
     return redirect(url_for("login"))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # use Heroku’s port if available
+    app.run(host="0.0.0.0", port=port)
